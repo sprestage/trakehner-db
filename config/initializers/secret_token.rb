b98a5b432d46196b3d8ed2638cdda250125155f0
Susan Prestage <susan.prestage@gmail.com>
2013-10-29 10:51:29 -0700

    change 'or' to '||' in token file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,7 +9,7 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TrakehnerDb::Application.config.secret_key_base = if Rails.env.development? or Rails.env.test?
+TrakehnerDb::Application.config.secret_key_base || if Rails.env.development? or Rails.env.test?
     ('x' * 30) # meets minimum requirement of 30 chars long
   else
     ENV['RAILS_SECRET']
