459f784350219fd54834bf4794989356887b5bdc
NBarnes <nbarnes@gmail.com>
2013-11-01 09:11:26 -0700

    Fixed some logic in build_name_tree to, you know, work.

--- app/helpers/horses_helper.rb
+++ app/helpers/horses_helper.rb
@@ -16,48 +16,48 @@ class FamilyNameTree
   end
 
   def sire
-    return @name_tree[1]
+    return @name_tree[0]
   end
 
   def dam
-    return @name_tree[2]
+    return @name_tree[1]
   end
 
   def sires_sire
-    return @name_tree[3]
+    return @name_tree[2]
   end
 
   def sires_dam
-    return @name_tree[4]
+    return @name_tree[3]
   end
 
   def dams_sire
-    return @name_tree[5]
+    return @name_tree[4]
   end
 
   def dams_dam
-    return @name_tree[6]
+    return @name_tree[5]
   end
 
   def build_name_tree
 
     if @horse
       if @horse.sire
-        @name_tree[1] = @horse.sire.name
+        @name_tree[0] = @horse.sire.name
         if @horse.sire.sire
-          @name_tree[4] = @horse.sire.sire.name
+          @name_tree[2] = @horse.sire.sire.name
         end
         if @horse.sire.dam
-          @name_tree[3] = @horse.sire.sire.name
+          @name_tree[3] = @horse.sire.dam.name
         end
       end
       if @horse.dam
-          @name_tree[2] = @horse.sire.name
+          @name_tree[1] = @horse.dam.name
         if @horse.dam.sire
-          @name_tree[5]= @horse.sire.sire.name
+          @name_tree[4]= @horse.dam.sire.name
         end
         if @horse.dam.dam
-          @name_tree[6] = @horse.sire.sire.name
+          @name_tree[5] = @horse.dam.dam.name
         end
       end
     end
