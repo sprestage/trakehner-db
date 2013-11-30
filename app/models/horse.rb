5b467adddc655455518e55f98b66c32022cf6bb1
NBarnes <nbarnes@gmail.com>
2013-11-29 22:12:57 -0800

    Moved the parse-from-json code from import_horse_from_json_test into Horse.rb itself

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -30,4 +30,46 @@ class Horse < ActiveRecord::Base
     return self.default_dam || Horse.new(name: 'Unknown')
   end
 
+  def self.create_from_json(record)
+
+    horse = Horse.find_by name: record["name"]
+    unless horse
+      horse = Horse.new
+    end
+
+    horse.name = record["name"]
+    horse.registration_number = record["registration_number"]
+    horse.sex = record["sex"]
+    horse.color = record["color"]
+    horse.birth_year = record["birth_year"]
+
+    sire = Horse.find_by name: record["sire"]
+    if sire
+      horse.sire = sire
+    else
+      sire = Horse.new(name: record["sire"])
+      sire.save!
+      horse.sire = sire
+    end
+
+    dam = Horse.find_by name: record["dam"]
+    if dam
+      horse.dam = dam
+    else
+      dam = Horse.new(name: record["dam"])
+      dam.save!
+      horse.dam = dam
+    end
+
+    horse.breeder = record["breeder"]
+    horse.performance_records_available = record["performance_records_available"]
+
+    if horse.save
+      # Do nothing if the save is successful.
+    else
+      puts "Save to database failed!!!"
+    end
+
+  end
+
 end
--- test/features/import/import_horse_from_json_test.rb
+++ test/features/import/import_horse_from_json_test.rb
@@ -7,45 +7,7 @@ feature "Import horse" do
 
     records = JSON.parse(File.read('test/features/import/import_test_data/import_test_data.json'))
     records.each do |record|
-
-      horse = Horse.find_by name: record["name"]
-
-      unless horse
-        horse = Horse.new
-      end
-
-      horse.name = record["name"]
-      horse.registration_number = record["registration_number"]
-      horse.sex = record["sex"]
-      horse.color = record["color"]
-      horse.birth_year = record["birth_year"]
-
-      sire = Horse.find_by name: record["sire"]
-      if sire
-        horse.sire = sire
-      else
-        sire = Horse.new(name: record["sire"])
-        sire.save!
-        horse.sire = sire
-      end
-
-      dam = Horse.find_by name: record["dam"]
-      if dam
-        horse.dam = dam
-      else
-        dam = Horse.new(name: record["dam"])
-        dam.save!
-        horse.dam = dam
-      end
-
-      horse.breeder = record["breeder"]
-      horse.performance_records_available = record["performance_records_available"]
-
-      if horse.save
-        # Do nothing if the save is successful.
-      else
-        puts "Save to database failed!!!"
-      end
+      Horse.create_from_json(record)
     end
     # Then a new horse(s) should have been created and displayed
     visit horses_path
