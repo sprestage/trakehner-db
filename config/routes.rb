1d676399f2c79470db04eeaa1f06536092f6c09f
Susan Prestage <susan.prestage@gmail.com>
2013-11-30 14:17:38 -0800

    add static about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ TrakehnerDb::Application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    # get "signup", :to => "devise/registrations#new"
     get "signout", :to => "devise/sessions#destroy"
-    # get "signin", :to => "devise/sessions#new"
   end
 
   resources :horses
 
   # get "home/index"
   root :to => 'home#index'
+
+  get ':action' => 'static#:action'
 
 end
