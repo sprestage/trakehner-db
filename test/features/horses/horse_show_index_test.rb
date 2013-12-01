27c02c616164d21c31186c96d0fd9d434b9f4ad3
Susan Prestage <susan.prestage@gmail.com>
2013-12-01 12:13:38 -0800

    test for pagination on horses index

--- test/features/horses/horse_show_index_test.rb
+++ test/features/horses/horse_show_index_test.rb
@@ -6,8 +6,18 @@ feature "Show horse index" do
     # When I visit the horses path
     visit horses_path
     # Then I should see all horses
-    page.text.must_include horses(:herzminze).name
-    page.text.must_include horses(:indiana3).name
+    page.text.must_include "Abdullah"
+    page.text.must_include "Abiza"
+  end
+
+  scenario "successfully see pagination of horses in the index" do
+    # Given existing horses
+    # When I visit the horses path
+    visit horses_path
+    # Then I should see pagination links and only see the first 25 horses alphabetically
+    page.text.must_include "Next"
+    page.text.must_include "Last"
+    page.wont_have_content horses(:polarpunkt).name
   end
 
 end
