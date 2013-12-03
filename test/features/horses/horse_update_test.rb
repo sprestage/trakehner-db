c4e74d9801c943ba53980b1b8f1f0012c4ab45fe
Susan Prestage <susan.prestage@gmail.com>
2013-12-02 16:34:59 -0800

    fix horse update test

--- test/features/horses/horse_update_test.rb
+++ test/features/horses/horse_update_test.rb
@@ -16,36 +16,38 @@ feature "Update Horse" do
     page.text.must_include 'Horse was successfully updated'
   end
 
-  # scenario "fail to update horse, validation errror, name presence" do
-  #   # Given an existing horse
-  #   sign_in
-  #   visit horse_path(horses(:horse02))
-  #   page.text.must_include 'Edit'
-  #   click_on 'Edit'
-  #   fill_in 'Name', with: ""
-  #   # When I click edit and submit changed data
-  #   click_on 'Update Horse'
-  #   # Then I should see failure message
-  #   page.text.must_include "Name can't be blank"
-  #   # and no updated horse or success message
-  #   page.wont_have_content "Horse was successfully updated"
-  #   page.wont_have_content "Second Horse"
-  # end
+  scenario "fail to update horse, validation errror, name presence" do
+    # Given an existing horse
+    sign_in
+    visit horse_path(horses(:horse02))
+    page.text.must_include 'Edit'
+    click_on 'Edit'
+    fill_in 'Name', with: ""
+    # When I click edit and submit changed data
+    click_on 'Update Horse'
+    # Then I should see failure message
+    page.text.must_include "Name can't be blank"
+    # and no success message
+    page.wont_have_content "Horse was successfully updated"
+    # and stilll on edit page instead of show page
+    page.text.must_include "Editing horse"
+  end
 
-  # scenario "fail to update horse, failed validation, name uniqueness" do
-  #   # Given an existing horse
-  #   sign_in
-  #   visit horse_path(horses(:horse02))
-  #   page.text.must_include 'Edit'
-  #   click_on 'Edit'
-  #   fill_in "Name", with: horses(:horse04).name
-  #   # When the form is submitted
-  #   click_on "Update Horse"
-  #   # Then I should see failure message
-  #   page.text.must_include "Name has already been taken"
-  #   # and no updated horse or success message
-  #   page.wont_have_content "Horse was successfully created"
-  #   page.wont_have_content horses(:horse04).name
-  # end
+  scenario "fail to update horse, failed validation, name uniqueness" do
+    # Given an existing horse
+    sign_in
+    visit horse_path(horses(:horse02))
+    page.text.must_include 'Edit'
+    click_on 'Edit'
+    fill_in "Name", with: horses(:horse04).name
+    # When the form is submitted
+    click_on "Update Horse"
+    # Then I should see failure message
+    page.text.must_include "Name has already been taken"
+    # and no success message
+    page.wont_have_content "Horse was successfully updated"
+    # and stilll on edit page instead of show page
+    page.text.must_include "Editing horse"
+  end
 
 end
