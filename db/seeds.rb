2b21823c9410c2d8fcf0be40c93eb6ed19f32d6f
NBarnes <nbarnes@gmail.com>
2013-12-02 21:08:40 -0800

    Uncommented section of seeds.rb to fully load the JSON horse data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# ('a'..'z').each do |initial|
-#   puts "Importing horses with initial #{ initial }"
-#   filename = "app/assets/data/json/horses_from_ATA_site/horses_initial_#{ initial }.json"
-#   records = JSON.parse(File.read(filename))
-#   records.each do |record|
-#     Horse.create_from_json(record)
-#   end
-# end
+('a'..'z').each do |initial|
+  puts "Importing horses with initial #{ initial }"
+  filename = "app/assets/data/json/horses_from_ATA_site/horses_initial_#{ initial }.json"
+  records = JSON.parse(File.read(filename))
+  records.each do |record|
+    Horse.create_from_json(record)
+  end
+end
 
 puts "Importing stallion images"
 filename = "app/assets/data/json/ata_2013_approved_stallions.json"
