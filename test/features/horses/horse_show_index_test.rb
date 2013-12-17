60042c9a2a3c69c3bf91d1ae6963668ca5225507
Susan Prestage <susan.prestage@gmail.com>
2013-12-17 07:55:32 -0800

    add pagination test

--- test/features/horses/horse_show_index_test.rb
+++ test/features/horses/horse_show_index_test.rb
@@ -6,8 +6,8 @@ feature "Show horse index" do
     # When I visit the horses path
     visit horses_path
     # Then I should see all horses
-    page.text.must_include "Abdullah"
-    page.text.must_include "Abiza"
+    page.text.must_include horses(:abdullah).name
+    page.text.must_include horses(:abiza).name
   end
 
   scenario "logged in admin also successfully sees all horses in the index" do
@@ -16,8 +16,8 @@ feature "Show horse index" do
     # When I visit the horses path
     visit horses_path
     # Then I should see pagination links and only see the first 25 horses alphabetically
-    page.text.must_include "Abdullah"
-    page.text.must_include "Abiza"
+    page.text.must_include horses(:abdullah).name
+    page.text.must_include horses(:abiza).name
   end
 
   scenario "successfully see pagination of horses in the index" do
@@ -28,6 +28,18 @@ feature "Show horse index" do
     page.text.must_include "Next"
     page.text.must_include "Last"
     page.wont_have_content horses(:zzz).name
+  end
+
+  scenario "successfully follow pagination of horses in the index" do
+    # Given existing horses
+    # When I visit the horses path, I only see the first 25 horses alphabetically
+    visit horses_path
+    page.text.must_include horses(:abdullah).name
+    page.wont_have_content horses(:zzz).name
+    # Then follow pagination link and only see the next 25 horses alphabetically
+    click_on "Next"
+    page.text.must_include horses(:zzz).name
+    page.wont_have_content horses(:abdullah).name
   end
 
 end
