fec4f36b6eef186ddd7c9683dd0a7ded10ed8f37
NBarnes <nbarnes@gmail.com>
2014-03-07 14:59:51 -0800

    Maybe the i18n reset bug is fixed?  Hard to know when I can't replicate locally.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
 
   def default_url_options(options = {})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    { locale: I18n.locale == I18n.default_locale ? nil :I18n.locale }
+    { locale: I18n.locale }
   end
 
   protected
