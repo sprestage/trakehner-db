7d8d0eb520cf467281be1fc48541fb4338aa0460
nbarnes <nbarnes@pirq.com>
2015-08-29 15:36:08 -0700

    Updates to Carrierwave configuration.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,9 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'us-west-2',
+    endpoint: 'http://s3.amazonaws.com'
   }
   config.fog_directory = "unofficialtrakehnerdb"
 end
