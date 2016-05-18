d2412af918bbca92aafc0f2845e2be6480e35838
Susan Prestage <susan.prestage@gmail.com>
2016-05-18 12:14:40 -0700

    Changes the AWS endpoint for S3 images.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'us-west-2',
-    endpoint: 'http://s3.amazonaws.com'
+    endpoint: 'http://s3-us-west-2.amazonaws.com'
   }
   config.fog_directory = "unofficialtrakehnerdb"
 end
